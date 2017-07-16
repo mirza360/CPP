a9eca2df476886a74d85518b08a5e7451b4648c6
Mirza Belal Ahmed <mirza360@hotmail.com>
2017-07-16 23:52:26 +0600

    All The Tasks are done

--- fileOp/fileOp/fileOp.cpp
+++ fileOp/fileOp/fileOp.cpp
@@ -32,30 +32,132 @@ struct Node {
 	//for active list
 };
 //Structure related global variables
-Node *arr[pNum];
+
 
 //Functions area
-void tokenize(string st);
-void insert(string st);
-void print();
+string tokenize(string st);
+//void insert(string st);
+//void print();
 //void brunching(Node *br, Node *n);
-
+Node *arr[pNum];
 //Main function
-int main(/*char *argv[]*/)
+int main()
 {
 	string line;
+	//string word="";
 	ifstream myfile;
-	myfile.open("file.txt");
-	if (myfile.is_open())
-	{
-		while (getline(myfile, line))
-		{
-			//cout << line<<endl;
-			tokenize(line);
+	cout << "Enter the .txt file name: ";
+	string fileName;
+	cin >> fileName;
+	myfile.open(fileName);
+	if (myfile) {
+		while (myfile >> line) {
+			//cout << line << endl;
+			string st = tokenize(line);
+			wCount++;
+			//cout << word<<endl;
+			//testing starts
+
+			int res = 0;
+			for (int idx = 0; idx < (int)st.size(); idx++) {
+				res += (int)st.at(idx);
+			}
+			res = res%pNum;
+			//cout << "Key Value: " << res << " ";
+			//conversion to number ends
+			//--create the word node
+			Node *wrd = new Node();
+			wrd->key = st;
+			wrd->total++;
+			//-position side node
+			Node*pos = new Node();
+			pos->pos = wCount;
+			//cout << "Current Position" << pos->pos << endl;
+			/*
+			wrd->side = pos;
+			wrd->last = pos;*/
+			//-end of position side node
+			//--end of word node
+			if (arr[res] == NULL) {
+				arr[res] = new Node();
+			}
+			//if has table index empty
+			if (!arr[res]->begin) {
+				arr[res]->begin = true;
+				arr[res]->next = wrd;
+				wrd->side = pos;
+				wrd->last = pos;
+				//cout << "begin 1" << endl;
+
+			}//if table index already has something
+			else {
+				//checking for duplicate
+				Node *idx = arr[res]->next;
+				bool check = false;//if duplicate found, it will be true
+				while (idx != NULL) {
+					//matching the key
+					if (idx->key.compare(st) == 0) {
+						//cout << idx->key;
+						//cout << "begin 2" << endl;
+						idx->last->side = pos;
+						idx->total++;
+						idx->last = pos;
+						//delete (wrd);
+						check = true;
+						break;
+					}//if not matched
+					else {
+						idx = idx->next;
+					}
+				}
+				if (check == false) {
+					idx = wrd;
+					wrd->side = pos;
+					wrd->last = pos;
+				}
+				//debugging
+				/*
+				Node *dx = arr[res]->next->side;
+				cout << arr[res]->next->total << endl;
+				while (dx != NULL) {
+				cout << dx->pos << ", ";
+				dx = dx->side;
+				}*/
+				//end of debugging
+			}
+
+			//testing ends
 		}
-		//print();
-		myfile.close();
-		
+		//print here
+		//cout << "Hello";
+		for (int idx = 0; idx < pNum; idx++) {
+			if (arr[idx]) {
+				
+				while (arr[idx]->next) {
+					//cout << arr[idx]->next->total;
+					float a = (float)(arr[idx]->next->total) / (float)(wCount);
+					
+					cout << arr[idx]->next->key << " - ";
+					printf("%.4f: ", a);
+					//cout << "";
+					while (arr[idx]->next->side) {
+						if (arr[idx]->next->side->side == NULL) {
+							cout << arr[idx]->next->side->pos << " ";
+						}
+						else {
+							cout << arr[idx]->next->side->pos << ",";
+						}
+						arr[idx]->next->side = arr[idx]->next->side->side;
+					}
+					cout << endl;
+
+					arr[idx]->next = arr[idx]->next->next;
+				}
+				//cout << "Hi";
+			}
+
+		}
+		//print ends
 	}
 	else {
 		cout << "Unable to open";
@@ -63,118 +165,24 @@ int main(/*char *argv[]*/)
 	return 0;
 }
 //Function implementation
-void tokenize(string st) {
+string tokenize(string st) {
 
-	cout << endl << st.size() << endl;
+	//cout << endl << st.size() << endl;
 	string word = "";
-	string snt = "";
+	//string snt = "";
 	//running a loop where each char of st will be assigned to c
+	char c;
 	for (int idx = 0; idx < (int)st.size(); idx++) {
-		if ((int)st.at(idx) >=0 && (int)st.at(idx) <= 255) {
-			
-
-			char c = st.at(idx);
-			c = tolower(c);
-			//dot operation
-			if (c != '.' && !isspace(c)  && !ispunct(c) && c!='\n') {
-				
-				word += c;
-			}
-			else if (c == '.' || c == ' ' || c == '\n') {
-
-				//got our word ready!
-
-				
-				if (!word.empty()) {
-					wCount++;
-					
-					cout << "Word # " << wCount << " : ";
-					cout << word << endl;
-					insert(word);
-					word.erase();
-				}
-
-			}
-
-	
-
+		c = st.at(idx);
+		if (!ispunct(c) && isalpha(c)) {
+			word += tolower(c);
 		}
 		
 	}
+	//cout << word;
+	return word;
 
 	
 }
 //insert operation
-void insert(string st) {
-	//converting into number
-	int res = 0;
-	for (int idx = 0; idx < (int)st.size(); idx++) {
-		res += (int)st.at(idx);
-	}
-	res = res%pNum;
-	cout << res<<endl;
-	//conversion to number ends
-	//--create the word node
-	Node *wrd = new Node();
-	wrd->key = st;
-	wrd->total++;
-	//-position side node
-	Node*pos = new Node();
-	pos->pos = wCount;
-	cout << pos->pos<<endl;
-	/*
-	wrd->side = pos;
-	wrd->last = pos;*/
-	//-end of position side node
-	//--end of word node
-	if (arr[res] == NULL) {
-		arr[res] = new Node();
-	}
-	//if has table index empty
-	if(!arr[res]->begin){
-		arr[res]->begin = true;
-		arr[res]->next = wrd;
-		wrd->side = pos;
-		wrd->last = pos;
-		cout << "begin 1" << endl;
-		
-	}//if table index already has something
-	else {
-		//checking for duplicate
-		Node *idx = arr[res]->next;
-		bool check = false;//if duplicate found, it will be true
-		while (idx != NULL) {
-			//matching the key
-			if (idx->key.compare(st)==0) {
-				//cout << idx->key;
-				cout << "begin 2" << endl;
-				idx->last->side = pos;
-				idx->total++;
-				idx->last = pos;
-				//delete (wrd);
-				check = true;
-				break;
-			}//if not matched
-			else {
-				idx = idx->next;
-			}
-		}
-		if (check == false) {
-			idx = wrd;
-			wrd->side = pos;
-			wrd->last = pos;
-		}
-		//debugging
-		Node *dx = arr[res]->next->side;
-		cout << arr[res]->next->total << endl;
-		while (dx != NULL) {
-			cout << dx->pos << ", ";
-			dx = dx->side;
-		}
-		//end of debugging
-	}
-		
-}
-void print() {
-	
-}
+
