40c4dc0627c5c6bf5dd60a46b083f155c25c8a92
Mirza Belal Ahmed <mirza360@hotmail.com>
2017-07-19 02:06:05 +0600

    BST Implementation
    Basic Operations

---
+++ lab15/lab15/main.cpp
@@ -0,0 +1,44 @@
+// lab15.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<cmath>
+int fib(int n);
+double series(int n);
+using namespace std;
+
+int main()
+{
+	/*cout << "Fibonacci n =";
+	int fibN;
+	cin >> fibN;
+	cout << fib(fibN);*/
+
+	cout << " n =";
+	int n;
+	cin >> n;
+	cout << series(n);
+    return 0;
+}
+int fib(int n) {
+	int nthFib = 0;
+	if (n <= 2) {
+		return 1;
+	}
+	else {
+		nthFib = fib(n - 1) + fib(n - 2);
+	}
+	return nthFib;
+}
+
+double series(int n) {
+	double sum=0.0;
+	if (n == 0) {
+		return 1;
+	}
+	else {
+		sum = (1 / pow(2, n)) + series(n - 1);
+	}
+	return sum;
+}
