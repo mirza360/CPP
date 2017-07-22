13a4765618ce2c60e562220c1262b266367f90aa
Mirza Belal Ahmed <mirza360@hotmail.com>
2017-07-23 01:54:22 +0600

    tree even odds

--- mid2/mid2/main.cpp
+++ mid2/mid2/main.cpp
@@ -12,10 +12,27 @@ struct Node {
 };
 Node *root = NULL;
 Node* TreeInsert(Node*tree, int v);
+void print(Node*tree);
+int printOdds(Node *tree);
 int main()
 {
+	root = TreeInsert(root, 55);
+	root = TreeInsert(root, 11);
+	root = TreeInsert(root, 22);
+	root = TreeInsert(root, 33);
+	root = TreeInsert(root, 44);
+	root = TreeInsert(root, 66);
+	root = TreeInsert(root, 77);
+	root = TreeInsert(root, 88);
+	root = TreeInsert(root, 99);
+	print(root);
+	cout << endl;
+	//printOdds(root);
+	int x = 0;
+	x=printOdds(root);
+	cout << x;
 
-    return 0;
+	return 0;
 }
 Node* TreeInsert(Node*tree, int v) {
 	if (tree == NULL) {
@@ -23,15 +40,47 @@ Node* TreeInsert(Node*tree, int v) {
 		n->value = v;
 		n->left = NULL;
 		n->right = NULL;
+		tree = n;
+		//cout << "HELLO";
 	}
 	else {
 		if (tree->value >= v) {
-			tree->right = TreeInsert(tree->right, v);
+			tree->left = TreeInsert(tree->left, v);
 		}
 		else {
-			tree->left = TreeInsert(tree->left, v);
+			tree->right = TreeInsert(tree->right, v);
 		}
 	}
 	return tree;
 }
 
+void print(Node*tree) {
+	if (tree == NULL) {
+		//cout << "No Value";
+	}
+	else {
+		print(tree->left);
+		cout << tree -> value<<" ";
+		print(tree->right);
+
+	}
+}
+int printOdds(Node *tree) {
+	int ret = 0;
+	if (tree) {
+		
+		if (tree->value % 2 == 1) {
+			ret= 1;
+		}
+		else {
+			ret= 0;
+		}
+		//printOdds(tree->right);
+		return ret + printOdds(tree->left) + printOdds(tree->right);
+	}
+	else {
+		return 0;
+	}
+	
+	
+}
